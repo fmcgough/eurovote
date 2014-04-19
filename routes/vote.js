0204b7ed7f1678cd0036ae592f9c00a839dce47a
Frankie McGough <fmcgough@live.co.uk>
2014-04-19 17:00:25 +0100

    Fetch countries from database instead of harcoding into vote.js

--- routes/vote.js
+++ routes/vote.js
@@ -3,24 +3,30 @@
  * GET voting page
  */
 
-var countries = {"1" : "Country1", 
-                "2" : "Country2", 
-                "3" : "Country3", 
-                "4" : "Country4", 
-                "5" : "Country5",
-                "6" : "Country6",
-                "7" : "Country7",
-                "8" : "Country8",
-                "9" : "Country9",
-                "10" : "Country10",
-                "11" : "Country11",
-                "12" : "Country12",
-                "13" : "Country13",
-                "14" : "Country14",
-                "15" : "Country15"};
+var pg = require('pg');
 
-exports.display = function(req, res){
-    res.render('vote', {title: 'Vote', 
+var connUrl = "pg://myuser:password@localhost:5432/eurovision";
+
+var countries;
+
+exports.display = function(req, res)
+{
+    pg.connect(connUrl, function(err, client, done)
+    {
+        if (err)
+        {
+            return console.error("Error getting a connection", err);
+        }
+        client.query("select id, name from countries", function(err, result)
+        {
+            done();
+            if (err)
+            {
+                return console.error("Error performing query", err);
+            }
+            res.render('vote', {title: 'Vote', 
                         navbarActive : 'Vote',
-                        countries : countries});
+                        countries : result.rows});
+        });
+    });
 };
