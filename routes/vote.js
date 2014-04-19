f28016e0104a1379535a90650b401f7192ab9ca4
Frankie McGough <fmcgough@live.co.uk>
2014-04-19 11:56:53 +0100

    Resized grid widget and made it display countries from response. Still not convinced this is  the best place for them as it's basically display logic, but will do for now.

--- routes/vote.js
+++ routes/vote.js
@@ -3,6 +3,24 @@
  * GET voting page
  */
 
+var countries = {"1" : "Country1", 
+                "2" : "Country2", 
+                "3" : "Country3", 
+                "4" : "Country4", 
+                "5" : "Country5",
+                "6" : "Country6",
+                "7" : "Country7",
+                "8" : "Country8",
+                "9" : "Country9",
+                "10" : "Country10",
+                "11" : "Country11",
+                "12" : "Country12",
+                "13" : "Country13",
+                "14" : "Country14",
+                "15" : "Country15"};
+
 exports.display = function(req, res){
-  res.render('vote', {title: 'Vote', navbarActive : 'Vote'});
+    res.render('vote', {title: 'Vote', 
+                        navbarActive : 'Vote',
+                        countries : countries});
 };
