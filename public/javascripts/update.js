4a815274396f1d9b3e0926c47bf477c070ff6d0c
Frankie McGough <fmcgough@live.co.uk>
2014-05-10 20:06:45 +0100

    ebase country images; restyled table on results page.
    Conflicts:
    	public/javascripts/update.js
    	views/index.jade

---
+++ public/javascripts/update.js
@@ -0,0 +1,20 @@
+$(function()
+{
+    now.receiveUpdate = function(data)
+    {
+        console.log("Received update: " + JSON.stringify(data));
+
+        var newrows;
+        for (var i = 0; i < data.length; i++)
+        {
+            newrows += "<tr><td>"
+                    + "<span><img src=\"/images/" + data[i].country + ".png\" class=\"flag\"></span>"
+                    + data[i].country
+                    + "</td><td>"
+                    + data[i].score
+                    + "</td></tr>";
+        }
+        $("#results tbody").html(newrows);
+    };
+
+});
