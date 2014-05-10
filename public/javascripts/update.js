cd1588b70d385384c87b747141b4303f449a0f3f
Frankie McGough <fmcgough@live.co.uk>
2014-05-10 12:31:31 +0100

    Added flags to voting list as well

--- public/javascripts/update.js
+++ public/javascripts/update.js
@@ -8,7 +8,7 @@ $(function()
         for (var i = 0; i < data.length; i++)
         {
             newrows += "<tr><td>"
-                    + "<span><img src=\"/images/" + data[i].country + ".png\" class=\"flag\"></span>"
+                    + "<span><img src=\"/images/" + data[i].country + ".png\" class=\"flag-small\"></span>"
                     + data[i].country
                     + "</td><td>"
                     + data[i].score
