a84cfd25694e227f0164888a87b5c56c4adcf1dd
Frankie McGough <fmcgough@live.co.uk>
2015-02-19 18:35:22 +0000

    Replace route loading method with a synchronous implementation to ensure that all routes are applied before the server is started

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,30 @@
-"use strict";
+var fs = require('fs'),
+    path = require("path"),
+    validFileTypes = ['js'];
 
-var fs = require("fs");
-var path = require("path");
-var recursive = require("recursive-readdir");
-var basename  = path.basename(module.filename);
-
-module.exports = function(app) {
-    recursive(__dirname, [".*", module.filename], function(err, files){
-        files.map(function(file){
-            var relative = path.relative(__dirname, file);
-            var moduleName = path.dirname(relative) +
-                    path.sep + path.basename(relative, ".js");
-            if (moduleName.slice(0, 2) != "./") {
-                moduleName = "./" + moduleName;
+var requireFiles = function (directory, app) {
+    fs.readdirSync(directory).forEach(function (fileName) {
+        // Recurse if directory
+        if(fs.lstatSync(directory + '/' + fileName).isDirectory()) {
+            requireFiles(directory + '/' + fileName, app);
+        } else {
+            // Skip this file
+            if(fileName === 'index.js' && directory === __dirname) {
+                return
             }
-            var route = require(moduleName);
+            // Skip unknown filetypes
+            if(validFileTypes.indexOf(fileName.split('.').pop()) === -1) {
+                return;
+            }
+
+            // Require the file
+            var route = require(directory + path.sep + fileName);
             if (typeof route === "function") {
                 route(app);
             }
-        });
+        }
     });
+}
+module.exports = function (app) {
+  requireFiles(__dirname, app);
 }
